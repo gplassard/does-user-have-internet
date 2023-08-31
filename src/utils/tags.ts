84ec76eff251abb7c8365a8a205ff8ec46057cd3
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-08-31 19:17:29 +0200

    feat(tags): add tags

--- src/utils/tags.ts
+++ src/utils/tags.ts
@@ -2,8 +2,10 @@ export interface BaseTags {
   server: string;
   scope: 'perso';
   env: 'prod';
+  app: string;
   team: string;
   provider: string;
+  repo: string;
 }
 export interface Tags extends BaseTags {
   [k: string]: string;
