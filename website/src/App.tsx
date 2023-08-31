bd32350bb4f88416b00729bfad09e60071432b90
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2023-08-31 20:37:46 +0200

    feat(website): create website (#7)

---
+++ website/src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './App.css'
+import  data from './assets/data/data.json';
+
+function App() {
+  return (
+    <>
+      <h1>Does $user have internet ?</h1>
+      <div className="card">
+          <table className="styled-table">
+              <thead>
+              <tr>
+                  <th>User</th>
+                  <th>Internet Status</th>
+                  <th>Since</th>
+              </tr>
+
+              </thead>
+          <tbody>
+          {Object.entries(data.users).map(([username, userData]) => {
+              return (
+                  <tr>
+              <td>{username}</td>
+              <td className={userData.state === 'Alert' ? 'status-ko': 'status-ok'}>{userData.state === 'Alert' ? 'DOWN ' : 'UP '}</td>
+              <td>{userData.since}</td>
+                  </tr>
+              )
+          })}
+          </tbody>
+          </table>
+          <p>
+              Last updated {data.lastUpdatedAt}
+          </p>
+      </div>
+    </>
+  )
+}
+
+export default App
