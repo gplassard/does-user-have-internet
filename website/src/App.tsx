63cdbc4d82313244b1552cd63dae2837c9184a05
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-09-01 20:12:27 +0200

    feat(front): improve ui

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,38 +1,38 @@
 import './App.css'
-import  data from './assets/data/data.json';
+import data from './assets/data/data.json';
 
 function App() {
-  return (
-    <>
-      <h1>Does $user have internet ?</h1>
-      <div className="card">
-          <table className="styled-table">
-              <thead>
-              <tr>
-                  <th>User</th>
-                  <th>Internet Status</th>
-                  <th>Since</th>
-              </tr>
+    return (
+        <>
+            <h1>Does $user have internet ?</h1>
+            <div className="card">
+                <table className="styled-table">
+                    <thead>
+                    <tr>
+                        <th>User</th>
+                        <th>Internet Status</th>
+                        <th>Since</th>
+                    </tr>
 
-              </thead>
-          <tbody>
-          {Object.entries(data.users).map(([username, userData]) => {
-              return (
-                  <tr>
-              <td>{username}</td>
-              <td className={userData.state === 'Alert' ? 'status-ko': 'status-ok'}>{userData.state === 'Alert' ? 'DOWN ' : 'UP '}</td>
-              <td>{userData.since}</td>
-                  </tr>
-              )
-          })}
-          </tbody>
-          </table>
-          <p>
-              Last updated {data.lastUpdatedAt}
-          </p>
-      </div>
-    </>
-  )
+                    </thead>
+                    <tbody>
+                    {Object.entries(data.users).map(([username, userData]) => {
+                        return (
+                            <tr>
+                                <td>{username}</td>
+                                <td className={userData.state === 'Alert' ? 'status-ko' : 'status-ok'}>{userData.state === 'Alert' ? '😱 DOWN 😱' : '🥳 UP 🥳'}</td>
+                                <td>{userData.since}</td>
+                            </tr>
+                        )
+                    })}
+                    </tbody>
+                </table>
+                <p>
+                    Last updated at {data.lastUpdatedAt} through a {data.trigger} trigger
+                </p>
+            </div>
+        </>
+    )
 }
 
 export default App
