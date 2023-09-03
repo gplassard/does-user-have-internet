8d3b0fcbdf9ab8eec913fceaa9a5527daab808d7
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2023-09-03 20:54:09 +0200

    build(release-rank): bump projen & specify release rank (#53)

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,6 +4,7 @@ import { TypescriptApplicationProject } from '@gplassard/projen-extensions';
 // opinionated wrapper around projen TypeScriptProject
 const project = new TypescriptApplicationProject({
   name: 'does-user-have-internet',
+  releaseRank: 3,
   devDeps: ['cdktf-cli', 'cdktf', 'constructs', '@cdktf/provider-aws', '@cdktf/provider-datadog'],
   peerDeps: ['cdktf', 'constructs', '@cdktf/provider-aws', '@cdktf/provider-datadog'],
   gitignore: ['*.tfstate*', 'cdktf.out'],
