9f9ad348fc9b04eb05c8b3ee3ff95de94a2026d3
Gabriel Plassard <gabriel.plassard@gmail.com>
2023-09-08 01:11:41 +0200

    docs(archi): fix architecture

--- docs/architecture/diagram.py
+++ docs/architecture/diagram.py
@@ -3,22 +3,22 @@ from diagrams.aws.management import ParameterStore
 from diagrams.onprem.iac import Terraform
 from diagrams.onprem.vcs import Github
 from diagrams.programming.framework import React
+from diagrams.programming.language import Typescript
 from diagrams.saas.logging import Datadog
 
-with (Diagram("Does user have internet ?", show=True, filename="docs/architecture/architecture")):
+with (Diagram("Does user have internet ?", show=False, filename="docs/architecture/architecture")):
     workflow = Datadog("Workflow")
-    pull_request = Github("Pull request")
+    pull_request = Github("Pull request with status files")
 
     ParameterStore("Configuration") \
-        >> Edge(label="is used as inputs") >> Terraform("Stack") \
-        >> Edge(label="deploys") >> Datadog("Monitors checking the users") \
+        >> Edge(label="is used as inputs") >> Typescript("CDK-TS") \
+        >> Edge(label="produces") >> Terraform("Stack") \
+        >> Edge(label="deploys") >> Datadog("Monitors verifying the connectivity") \
         >> Edge(label="triggers") >> workflow
 
     workflow \
-        >> Edge(label="creates") >> Github("Pull request with status files") \
-
-    workflow \
         >> Edge(label="creates") >> pull_request
+
     workflow \
         >> Edge(label="triggers") >> Github("Github Action") \
         >> Edge(label="merges") >> pull_request \
