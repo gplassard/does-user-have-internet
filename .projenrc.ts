66927639a9d1f609dad9c43af156409cb601d808
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2026-02-15 19:45:58 +0100

    chore(purge): add purge workflow

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,5 +1,6 @@
 // .projenrc.ts
 import { TypescriptApplicationProject, NODEJS_VERSIONS } from '@gplassard/projen-extensions';
+import { JobPermission } from 'projen/lib/github/workflows-model';
 
 // opinionated wrapper around projen TypeScriptProject
 const project = new TypescriptApplicationProject({
@@ -9,6 +10,29 @@ const project = new TypescriptApplicationProject({
   peerDeps: ['cdktf-cli', 'cdktf', 'constructs', '@cdktf/provider-aws', '@cdktf/provider-datadog', '@gplassard/cdktf-extensions'],
   gitignore: ['*.tfstate*', 'cdktf.out'],
   nodeVersion: NODEJS_VERSIONS.NODEJS_20_X,
+});
+
+const cleanupWorkflow = project.github?.addWorkflow('cleanup');
+cleanupWorkflow?.on({
+  schedule: [{ cron: '0 0 * * *' }],
+  workflowDispatch: {},
+});
+cleanupWorkflow?.addJobs({
+  cleanup: {
+    runsOn: ['ubuntu-latest'],
+    permissions: {
+      actions: JobPermission.WRITE,
+    },
+    steps: [
+      {
+        name: 'Delete old workflow runs',
+        run: 'gh run list --repo ${{ github.repository }} --limit 100 --json databaseId -q ".[].databaseId" | sed "1,10d" | xargs -I {} gh run delete --repo ${{ github.repository }} {}',
+        env: {
+          GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}',
+        },
+      },
+    ],
+  },
 });
 project.addScripts({
   cdktf: 'cdktf',
